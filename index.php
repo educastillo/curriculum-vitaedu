c20fd00355995997a25e6e7bc57a9d3f7a39acd5
Edu Castillo <152t0205@itsm.edu.mx>
2019-07-10 17:36:13 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,78 @@
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Eduardo</title>
+        <meta charset="utf-8">
+        <link rel="shorcut icon" type="image/x-icon" href="h.ico">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link href="https://fonts.googleapis.com/css?family=pacifico" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lato:200,300" rel="stylesheet" type="text/css">
+        
+    </head>
+    
+    <body>
+        <div id="header">
+        
+                
+            <img src="yo.jpg" alt="Yo" id="yo">
+            <h1 id="nombre">Vargas Castillo<br> Sergio Eduardo </h1>
+            <h2 class="datos"><a href="152t0205@itsm.edu.mx?subject=free phone">152t0205@itsm.edu.mx <img src="link.png"
+                alt="link" class="link"></a> <p> Domicilio calle Jose Diaz n.8 Fracc Lucas Martin Xalapa,Ver.</h2>
+                <h2 class="datos">2282537559</h2>
+                <h2 class="datos"><a href="https://www.facebook.com/" target="_blank" onmouseover="perfil de facebook"><img src="fb.png" alt="facebook logo" id="fb">Edu Castillo<img1 src="link.png" alt="link" class="link"></a></h2>
+            
+            
+        
+        <div id="info">
+        
+            <h2 class="titulo">Carrera</h2>
+            <p class="contenido"> Ingenieria en sistemas computacionales</p>
+            
+            <h2 class="titulo">Universidad</h2>
+            <p class="contenido"> Instituto Tecnologico Superior de Misantla</p>
+            
+            <h2 class="titulo">Estudios Realizados</h2>
+            <p class="contenido">  * curso de java script<br>
+            * curso paqueteria de office<br>
+            *curso de imagenes en 3D<br>
+            *curso de html<br>
+            </p>
+            
+            <h2 class="titulo">peliculas </h2>
+            <p class="contenido"> *Coco Disney-Pixar<br>
+            *Perfectos desconocidos<br>
+			*detroit<br>
+           
+			
+			 <h2 class="titulo">Lenguagues de programacion </h2>
+            *html<br>
+            *lenguaje ensamblador<br>
+			*android<br>
+            </p>
+            
+            <h2 class="titulo">Experiencia Laboral</h2>
+            <p class="contenido">chef administrativo <br>
+			    
+            </p>
+            
+            <h2 class="titulo">proyectos</h2>
+            <p class="contenido">*impresora 3D<br>
+            *sitio web Estacion Gamer Xalapa<br>
+            </p>           
+        
+        </div>
+        
+                    
+                    
+        <footer>
+            
+            
+       
+            <P>
+                * mi experiencia personal y profesional me permite desarrollar eficientemente mecanismos de incremento productivo y mejoras económicas.”*
+            </P>
+            <p>TODOS LOS DERECHOS RESESVADOS Sergio Eduardo Vargas Castillo Ing.Sistemas  Computacionales</p>
+        </footer
+          
+</body>
+</html>
